d7be3e9e36e1d34c31ccc75dc382f0362e864c05
dedumid-max <dedumid2009312@gmail.com>
2026-07-13 16:14:17 +0500

    Add files via upload

--- data.js
+++ data.js
@@ -1,67 +1,33 @@
 // ===== DATA.JS =====
 
-// Ochkolar
-let coins = Number(localStorage.getItem("coins")) || 589;
+// Coin
+let coins = Number(localStorage.getItem("coins")) || 0;
 
-// Birinchi spin
-let firstSpin = localStorage.getItem("firstSpin");
-if (firstSpin === null) {
-    firstSpin = true;
-} else {
-    firstSpin = false;
-}
-
-// Oxirgi spin vaqti
+// Oxirgi aylantirish vaqti
 let lastSpin = Number(localStorage.getItem("lastSpin")) || 0;
 
-// Jami spinlar
-let spinCount = Number(localStorage.getItem("spinCount")) || 0;
-
-// 24 soat (millisekund)
+// 24 soat
 const SPIN_TIME = 24 * 60 * 60 * 1000;
 
-// Ehtimollar
+// Sovrinlar
 const rewards = [
-    { value: 20, chance: 50 },
-    { value: 30, chance: 40 },
-    { value: 40, chance: 8 },
-    { value: 50, chance: 1 },
-    { value: 60, chance: 0.94 },
-    { value: 70, chance: 0.05 },
-    { value: 100, chance: 0.01 }
+    20,
+    30,
+    40,
+    50,
+    60,
+    70,
+    100,
+    200
 ];
 
-// Tasodifiy sovrin
-function randomReward() {
-
-    if (firstSpin) {
-        firstSpin = false;
-        localStorage.setItem("firstSpin", "done");
-        return 100;
-    }
-
-    let r = Math.random() * 100;
-    let total = 0;
-
-    for (let item of rewards) {
-        total += item.chance;
-
-        if (r <= total) {
-            return item.value;
-        }
-    }
-
-    return 20;
-}
-
-// Saqlash
+// Ma'lumotni saqlash
 function saveData() {
     localStorage.setItem("coins", coins);
     localStorage.setItem("lastSpin", lastSpin);
-    localStorage.setItem("spinCount", spinCount);
 }
 
-// 24 soat tekshirish
+// Aylantirish mumkinmi?
 function canSpin() {
     return Date.now() - lastSpin >= SPIN_TIME;
 }
